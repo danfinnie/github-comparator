cab5d124b252fbd049dc43f8b87f6e426656357c
Dan Finnie <dan@danfinnie.com>
2014-05-12 00:53:40 -0400

    Really simple v1

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,10 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
+  .controller('githubRow', ['$scope', 'githubService', function($scope, githubService) {
+    $scope.$watch('repo', function(repo) {
+      githubService.getRepoInfo(repo).then(function (repoInfo) {
+        $scope.repoInfo = repoInfo;
+      });
+    });
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,17 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  service('githubService', ['$http', function ($http) {
+    this.getRepoInfo = function (repo) {
+      return $http.get('https://api.github.com/repos/' + repo, {
+        headers: {
+          'Accept': 'application/vnd.github.v3+json'
+        }
+      }).then(function (result) {
+        return {
+          forks: result.data.forks,
+          watchers: result.data.watchers
+        };
+      });
+    };
+  }]);
